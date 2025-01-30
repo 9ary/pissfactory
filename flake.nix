832e918af3fedc9bf262ddf4cfc794bed8177c05
novenary <novenary@kwak.zip>
2025-01-30 12:39:30 +0200

    Turn packages output into scope

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
         fn rec {
           inherit system;
           pkgs = inputs.nixpkgs.legacyPackages.${system};
-          packages = inputs.self.packages.${system};
         });
     systems = lib.systems.flakeExposed;
   in {
@@ -43,80 +42,82 @@
         }) {};
     });
 
-    packages = forAllSystems ({
-      pkgs,
-      packages,
-      ...
-    }: {
-      modcache = pkgs.linkFarmFromDrvs "modcache" (
-        builtins.map (mod:
-          pkgs.fetchurl {
-            url = mod.downloadUrl;
-            sha1 = (builtins.elemAt (builtins.filter (v: v.algo == 1) mod.hashes) 0).value;
-          }) (builtins.fromJSON (builtins.readFile ./mods.json))
-      );
+    packages = forAllSystems ({pkgs, ...}:
+      lib.makeScope pkgs.newScope (self: let
+        inherit (self) callPackage;
+      in {
+        modcache = callPackage ({
+          linkFarmFromDrvs,
+          fetchurl,
+        }: (pkgs.linkFarmFromDrvs "modcache" (
+          builtins.map (mod:
+            pkgs.fetchurl {
+              url = mod.downloadUrl;
+              sha1 = (builtins.elemAt (builtins.filter (v: v.algo == 1) mod.hashes) 0).value;
+            }) (builtins.fromJSON (builtins.readFile ./mods.json))
+        ))) {};
 
-      pack = pkgs.callPackage ({
-        difficulty ? "normal",
-        stdenvNoCC,
-        nodejs,
-        zip,
-        unzip,
-        jq,
-        packwiz,
-        python3,
-      }:
-        stdenvNoCC.mkDerivation (finalAttrs: {
-          name = "Monifactory-${difficulty}";
-          src = inputs.monifactory;
+        pack = callPackage ({
+          difficulty ? "normal",
+          stdenvNoCC,
+          nodejs,
+          zip,
+          unzip,
+          jq,
+          packwiz,
+          python3,
+        }:
+          stdenvNoCC.mkDerivation (finalAttrs: {
+            name = "Monifactory-${difficulty}";
+            src = inputs.monifactory;
 
-          nativeBuildInputs = [nodejs zip unzip jq packwiz python3];
+            nativeBuildInputs = [nodejs zip unzip jq packwiz python3];
 
-          env = {
-            CFCORE_API_TOKEN = "dummy";
-          };
+            env = {
+              CFCORE_API_TOKEN = "dummy";
+            };
 
-          postPatch = ''
-            patchShebangs --build .
-            cp -r '${./overlay}/.' .
-            rm config-overrides/*/difficultylock.json5
-          '';
-          dontConfigure = true;
-          buildPhase = ''
-            runHook preBuild
-            (
-              cd tools/build
-              node build.js -c build-client
-            )
-            runHook postBuild
-          '';
-          installPhase = ''
-            runHook preInstall
-            mkdir -p "$out"
-            unzip dist/client.zip -d "$out"
-            (
-              srcroot=$PWD
-              cd "$out"
-              (shopt -s dotglob; mv overrides/* .)
-              rmdir overrides
-              "$srcroot/pack-mode-switcher.sh" ${difficulty}
-              rm -r config-overrides manifest.json modlist.html
+            postPatch = ''
+              patchShebangs --build .
+              cp -r '${./overlay}/.' .
+              rm config-overrides/*/difficultylock.json5
+            '';
+            dontConfigure = true;
+            buildPhase = ''
+              runHook preBuild
+              (
+                cd tools/build
+                node build.js -c build-client
+              )
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out"
+              unzip dist/client.zip -d "$out"
+              (
+                srcroot=$PWD
+                cd "$out"
+                (shopt -s dotglob; mv overrides/* .)
+                rmdir overrides
+                "$srcroot/pack-mode-switcher.sh" ${difficulty}
+                rm -r config-overrides manifest.json modlist.html
 
-              # `packwiz init` tries to go online so we have to do this
-              substitute '${./pack.toml.in}' pack.toml \
-                --subst-var-by mc_version "$(jq -r '.minecraft.version' "$src/manifest.json")" \
-                --subst-var-by forge_version "$(jq -r '.minecraft.modLoaders[0].id | sub("^forge-"; "")' "$src/manifest.json")"
-              : > index.toml
-              python3 '${./gen_pw_mods.py}' '${./mods.json}'
-              packwiz refresh
-            )
-            (cd '${./bootstrap}'; zip -r "$out/pissfactory.zip" {,.}*)
-            runHook postInstall
-          '';
-        })) {};
-      pack-hardmode = packages.pack.override {difficulty = "hardmode";};
-      pack-expert = packages.pack.override {difficulty = "expert";};
-      default = packages.pack-hardmode;
-    });
+                # `packwiz init` tries to go online so we have to do this
+                substitute '${./pack.toml.in}' pack.toml \
+                  --subst-var-by mc_version "$(jq -r '.minecraft.version' "$src/manifest.json")" \
+                  --subst-var-by forge_version "$(jq -r '.minecraft.modLoaders[0].id | sub("^forge-"; "")' "$src/manifest.json")"
+                : > index.toml
+                python3 '${./gen_pw_mods.py}' '${./mods.json}'
+                packwiz refresh
+              )
+              (cd '${./bootstrap}'; zip -r "$out/pissfactory.zip" {,.}*)
+              runHook postInstall
+            '';
+          })) {};
+        pack-hardmode = self.pack.override {difficulty = "hardmode";};
+        pack-expert = self.pack.override {difficulty = "expert";};
+        default = self.pack-hardmode;
+      }));
   };
 }
