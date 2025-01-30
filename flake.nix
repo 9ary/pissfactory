30146a0639517b4eb12841d2b54337232b54389f
novenary <novenary@kwak.zip>
2025-01-30 13:19:25 +0200

    Add Forge server package

--- flake.nix
+++ flake.nix
@@ -118,6 +118,37 @@
         pack-hardmode = self.pack.override {difficulty = "hardmode";};
         pack-expert = self.pack.override {difficulty = "expert";};
         default = self.pack-hardmode;
+
+        forgeServer = callPackage ({
+          stdenvNoCC,
+          fetchurl,
+          jdk17_headless,
+        }:
+          stdenvNoCC.mkDerivation (finalAttrs: {
+            pname = "forge-server";
+            version = "1.20.1-47.3.7";
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+            outputHash = "sha256-kZlxL54b/PZGdrxnN3U4NrgixRKN1eqwZ4TeRKvSpYw=";
+
+            nativeBuildInputs = [jdk17_headless];
+
+            src = fetchurl {
+              url = "https://maven.minecraftforge.net/net/minecraftforge/forge/${finalAttrs.version}/forge-${finalAttrs.version}-installer.jar";
+              hash = "sha256-79OgS8Z/VXLWtGxzQBAydF8VuFCv1gi+fDSjLEqBrlU=";
+            };
+
+            dontUnpack = true;
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out"
+              cd "$out"
+              java -jar "$src" --installServer
+              rm *.log
+              runHook postInstall
+            '';
+            dontPatchShebangs = true;
+          })) {};
       }));
   };
 }
