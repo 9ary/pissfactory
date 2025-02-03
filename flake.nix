bec38f4e4164ca5bcc0f2af2e497e0e2a9a8bf92
novenary <novenary@kwak.zip>
2025-02-03 10:32:42 +0200

    Minor cleanup

--- flake.nix
+++ flake.nix
@@ -11,7 +11,19 @@
 
   outputs = inputs: let
     inherit (inputs.nixpkgs) lib;
-    inherit (lib) escapeShellArg genAttrs stringLength substring;
+    inherit
+      (lib)
+      elemAt
+      escapeShellArg
+      filter
+      fromJSON
+      genAttrs
+      makeScope
+      map
+      readFile
+      stringLength
+      substring
+      ;
     escapeStorePath = p: escapeShellArg (substring 0 (stringLength p) p);
     forAllSystems = fn:
       genAttrs systems (system:
@@ -44,18 +56,18 @@
     });
 
     packages = forAllSystems ({pkgs, ...}:
-      lib.makeScope pkgs.newScope (self: let
+      makeScope pkgs.newScope (self: let
         inherit (self) callPackage;
       in {
         modcache = callPackage ({
           linkFarmFromDrvs,
           fetchurl,
         }: (pkgs.linkFarmFromDrvs "modcache" (
-          builtins.map (mod:
+          map (mod:
             pkgs.fetchurl {
               url = mod.downloadUrl;
-              sha1 = (builtins.elemAt (builtins.filter (v: v.algo == 1) mod.hashes) 0).value;
-            }) (builtins.fromJSON (builtins.readFile ./mods.json))
+              sha1 = (elemAt (filter (v: v.algo == 1) mod.hashes) 0).value;
+            }) (fromJSON (readFile ./mods.json))
         ))) {};
 
         pack = callPackage ({
