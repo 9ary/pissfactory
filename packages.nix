2622892d78a656d2ee655b09ad86475a0a6d5399
novenary <novenary@kwak.zip>
2025-02-03 14:09:00 +0200

    Remove unstable keys from mods.json

--- packages.nix
+++ packages.nix
@@ -37,7 +37,7 @@ lib.makeScope newScope (
             --header "X-Api-Key: $CFCORE_API_TOKEN" \
             --header 'Content-Type: application/json' \
             --data "$(jq -n '{"fileIds": [inputs.files[].fileID]}' ${escapeStorePath pack.src}/manifest.json ${escapeStorePath ./manifest_extras.json})" \
-            | jq '.data | unique | sort_by(.modId) | map(.downloadUrl = (.downloadUrl // "https://edge.forgecdn.net/files/\(.id / 1000 | trunc)/\(.id % 1000)/\(.fileName)"))' \
+            | jq '.data | unique | sort_by(.modId) | map(.downloadUrl = (.downloadUrl // "https://edge.forgecdn.net/files/\(.id / 1000 | trunc)/\(.id % 1000)/\(.fileName)")) | map(del(.downloadCount, .gameVersions, .sortableGameVersions))' \
             > mods.json
         '';
         runtimeInputs = [curl jq];
