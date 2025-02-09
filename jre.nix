be271b9a47d7f8b5378432cf09f822756ace189c
novenary <novenary@kwak.zip>
2025-02-09 23:51:39 +0200

    Experiment: remove unneeded Java modules

--- jre.nix
+++ jre.nix
@@ -25,6 +25,12 @@ in
       ln -s ${jdk}/lib/openjdk/jmods/* modpath
       # jpackage includes path references to the base JDK in some native binaries
       rm modpath/jdk.jpackage.jmod
+      # Additional size reduction
+      rm modpath/jdk.hotspot.agent.jmod
+      rm modpath/jdk.localedata.jmod
+      rm modpath/{jdk.*compiler*,jdk.jdeps,jdk.jlink,jdk.jshell,jdk.javadoc}.jmod
+      rm modpath/jdk.charsets.jmod
+      rm modpath/jdk.incubator.*.jmod
       jlink --module-path modpath --add-modules ALL-MODULE-PATH --compress=2 --output $out
 
       runHook postBuild
