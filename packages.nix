ef2f31de35b9661c6408dde21cd7c24f632676fc
novenary <novenary@kwak.zip>
2025-02-13 21:50:35 +0200

    Introduce output overlay

--- packages.nix
+++ packages.nix
@@ -122,13 +122,14 @@ lib.makeScope newScope (
             "$srcroot/pack-mode-switcher.sh" ${difficulty}
             rm -r config-overrides manifest.json modlist.html
 
+            cp -r ${escapeStorePath ./out_overlay}/. .
             # `packwiz init` tries to go online so we have to do this
-            substitute ${escapeStorePath ./packwiz/pack.toml.in} pack.toml \
+            substituteInPlace pack.toml \
               --subst-var-by mc_version "$(jq -r '.minecraft.version' "$src/manifest.json")" \
               --subst-var-by forge_version "$(jq -r '.minecraft.modLoaders[0].id | sub("^forge-"; "")' "$src/manifest.json")"
             : > index.toml
-            python3 ${escapeStorePath ./packwiz/gen_pw_mods.py} ${escapeStorePath ./mods.json}
-            substitute ${escapeStorePath ./packwiz/forge-installer.pw.toml.in} forge-installer.pw.toml \
+            python3 ${escapeStorePath ./gen_pw_mods.py} ${escapeStorePath ./mods.json}
+            substituteInPlace forge-installer.pw.toml \
               --subst-var-by url ${escapeShellArg forgeServer.src.url} \
               --subst-var-by hash ${escapeShellArg forgeServer.src.outputHash}
             packwiz refresh
