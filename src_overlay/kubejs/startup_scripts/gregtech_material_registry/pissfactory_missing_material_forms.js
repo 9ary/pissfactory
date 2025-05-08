cb26b338ba169e4f127f2404361f55dd36525047
novenary <novenary@kwak.zip>
2025-05-08 23:27:42 +0300

    Enable netherite parts for tools

---
+++ src_overlay/kubejs/startup_scripts/gregtech_material_registry/pissfactory_missing_material_forms.js
@@ -0,0 +1,4 @@
+GTCEuStartupEvents.registry('gtceu:material', event => {
+    // Needed to craft tools
+    GTMaterials.Netherite.addFlags(GTMaterialFlags.GENERATE_BOLT_SCREW, GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_ROD)
+})
