7858865ea707bb4a0b5feb9b5fbba6ee10a1cfb4
Dusk Banks <me@bb010g.com>
2025-05-13 13:52:15 -0700

    add mod “Team Pin”

--- lock_mods.py
+++ lock_mods.py
@@ -113,6 +113,9 @@ cf_extras = {
     # Squake Reforged
     # https://www.curseforge.com/minecraft/mc-mods/squake-reforged
     1100654: 6368373,
+    # Team Pin
+    # https://www.curseforge.com/minecraft/mc-mods/team-pin
+    1094910: 5785806,
     # Tweakerge
     # https://www.curseforge.com/minecraft/mc-mods/tweakerge
     915857: 5633999,
