9cf5df408cba0214acce336adce89873866fa6a9
Dusk Banks <me@bb010g.com>
2025-11-19 01:00:34 -0800

    minecraft-site-static-root: prepare to serve multiple packs
    link pack to `./packs/pissfactory/minecraft` in addition to `.`.

--- nix/minecraft-site-static-root/nixpkgs-pissfactory-package.nix
+++ nix/minecraft-site-static-root/nixpkgs-pissfactory-package.nix
@@ -1,12 +1,27 @@
 { ... }:
 {
+  linkFarm,
   pack_hardmode,
   symlinkJoin,
   ...
 }:
+let
+  minecraft-site-static-pack =
+    { name, minecraft }:
+    linkFarm "minecraft-site-static-pack-${name}" [
+      {
+        name = "packs/${name}/minecraft";
+        path = minecraft;
+      }
+    ];
+in
 symlinkJoin {
   name = "minecraft-site-static-root";
   paths = [
     pack_hardmode
+    (minecraft-site-static-pack {
+      name = "pissfactory";
+      minecraft = pack_hardmode;
+    })
   ];
 }
